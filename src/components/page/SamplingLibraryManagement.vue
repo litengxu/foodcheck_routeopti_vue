2f0a8ef81c84bcde2d7a8018fef4fd6319f66a7c
litengxu <645425189@qq.com>
2021-01-05 22:18:31 +0800

    2020.1.5 抽检库一阶段基本完成

--- src/components/page/SamplingLibraryManagement.vue
+++ src/components/page/SamplingLibraryManagement.vue
@@ -17,10 +17,22 @@
                 >添加新的抽检库</el-button>
                 <el-button
                         type="primary"
+                        icon="el-icon-download"
+                        class="handle-del mr10"
+                        @click="downLoad"
+                >下载excel模板</el-button>
+                <el-button
+                        type="primary"
                         icon="el-icon-upload2"
                         class="handle-del mr10"
                         @click="uploadFormVisible = true"
-                >excel上传抽检库数据</el-button>
+                >利用excel上传抽检库数据</el-button>
+                <el-button
+                        type="primary"
+                        icon="el-icon-printer"
+                        class="handle-del mr10"
+                        @click="viewprocessingprogress"
+                >查看excel处理进度</el-button>
                 <el-input v-model="searchname" placeholder="抽检点名称" class="handle-input mr10"></el-input>
                 <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
             </div>
@@ -32,10 +44,9 @@
                     ref="multipleTable"
                     style="width: 100%"
                     header-cell-class-name="table-header"
-
                     @selection-change="handleSelectionChange"
-            >
 
+            >
                 <!--<el-table-column type="selection" width="55" align="center"></el-table-column>-->
                 <el-table-column type="expand">
                     <template slot-scope="props">
@@ -103,7 +114,6 @@
                 ></el-pagination>
             </div>
         </div>
-
         <!-- 编辑弹出框 -->
         <el-dialog v-dialogDrag title="编辑" :visible.sync="editVisible" width="30%">
             <el-form ref="form" :model="form" label-width="70px">
@@ -156,30 +166,39 @@
             </div>
         </el-dialog>
         <!-- 上传弹出框 -->
-        <el-dialog v-dialogDrag title="上传数据" :visible.sync="uploadFormVisible">
-            <el-form :model="addform"  :rules="rules">
-                <el-form-item prop="s_account"label="账号" :label-width="formLabelWidth" >
-                    <el-input v-model="addform.s_account" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item prop="s_password"label="密码" :label-width="formLabelWidth">
-                    <el-input v-model="addform.s_password" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item prop="s_username"label="用户名" :label-width="formLabelWidth">
-                    <el-input v-model="addform.s_username" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item prop="whether_participate"label="是否参与抽检" :label-width="formLabelWidth">
-                    <el-switch
-                            v-model="addform.whether_participate"
-                            active-text="参与"
-                            inactive-text="不参与">
-                    </el-switch>
-                </el-form-item>
-
-            </el-form>
-            <div slot="footer" class="dialog-footer">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="addnewsiinformation()">确 定</el-button>
-            </div>
+        <el-dialog v-dialogDrag title="上传数据(excel请务必按照模板格式)"   width="30%" :visible.sync="uploadFormVisible">
+            <el-upload
+                    class="upload-demo"
+                    :limit="3"
+                    ref="upload"
+                    action=""
+                    :on-preview="handlePreview"
+                    :on-remove="handleRemove"
+                    :file-list="fileList"
+                    :show-file-list="true"
+                    :http-request="uploadSectionFile"
+                    :on-exceed="fileexpandLimit"
+                    :auto-upload="false">
+                <!--:before-remove="handleRemove"-->
+                <el-button slot="trigger" size="small" type="primary" >选取文件</el-button>
+                <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到抽检库</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传excel文件，且不超过500kb</div>
+            </el-upload>
+            <!--<el-upload-->
+                    <!--style="display:inline-block"-->
+                    <!--:limit="5"-->
+                    <!--class="upload-demo"-->
+                    <!--ref="upload"-->
+                    <!--action=""-->
+                    <!--:file-list="fileList"-->
+                    <!--:http-request="uploadSectionFile"-->
+                    <!--:on-preview="handlePreview"-->
+                    <!--:on-remove="handleRemove"-->
+                    <!--:auto-upload="false"-->
+                    <!--:before-remove="handleRemove">-->
+                <!--<el-button slot="trigger" size="small" type="primary" plain>选取文件</el-button>-->
+                <!--<el-button style="margin-left: 10px;" size="small" icon="el-icon-upload2" type="success" @click="submitUpload">导入</el-button>-->
+            <!--</el-upload>-->
         </el-dialog>
         <!--食品类型管理弹出框-->
         <el-dialog v-dialogDrag title="食品类型管理" :visible.sync="foodtypeVisible" width="30%">
@@ -198,6 +217,82 @@
             <span slot="footer" class="dialog-footer">
                 <el-button @click="foodtypeVisible = false">取 消</el-button>
                 <el-button type="primary" @click="saveDistribute">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog title="excel处理进度" :visible.sync="processExcelVisible">
+
+            <el-table
+                    :data="processexceldata"
+                    border
+                    class="table"
+                    ref="multipleTable"
+                    style="width: 100%"
+                    header-cell-class-name="table-header"
+            >
+                <el-table-column type="expand">
+                    <template slot-scope="props">
+                        <el-form label-position="left" inline class="demo-table-expand">
+                            <el-form-item label="文件名">
+                                <span>{{ props.row.excel_name }}</span>
+                            </el-form-item>
+                            <el-form-item label="总数目">
+                                <span>{{ props.row.total }}</span>
+                            </el-form-item>
+                            <el-form-item label="成功数目">
+                                <span>{{ props.row.successtotal }}</span>
+                            </el-form-item>
+                            <el-form-item label="重复数目">
+                                <span>{{ props.row.repeattotal }}</span>
+                            </el-form-item>
+
+                            <el-form-item label="上传时间">
+                                <span>{{ props.row.create_time }}</span>
+                            </el-form-item>
+                            <el-form-item label="处理进度">
+                                <span>{{ props.row.state }}</span>
+                            </el-form-item>
+                        </el-form>
+                    </template>
+                </el-table-column>
+                <el-table-column sortable prop="excel_name" label="文件名"></el-table-column>
+                <el-table-column sortable prop="total" label="总数目" ></el-table-column>
+                <el-table-column sortable prop="successtotal" label="成功数目"></el-table-column>
+                <!--<el-table-column prop="repeattotal" label="重复数目"></el-table-column>-->
+                <!--<el-table-column prop="create_time" label="上传时间"></el-table-column>-->
+                <el-table-column label="处理进度" align="center">
+                    <template slot-scope="scope">
+                        <el-switch
+                                v-model="scope.row.state"
+                                :disabled="true"
+                                active-text="处理完成"
+                                inactive-text="正在处理">
+                        </el-switch>
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作" width="80" align="center">
+                    <template slot-scope="scope">
+                        <el-button
+                                type="text"
+                                icon="el-icon-delete"
+                                class="red"
+                                @click="handleprocessexcelDelete(scope.$index, scope.row)"
+                        >删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="pagination">
+                <el-pagination
+                        background
+                        layout="total, prev, pager, next"
+                        :current-page="processexcelquery.pageIndex"
+                        :page-size= "processexcelquery.pageSize"
+                        :total="processexcelquery.pageTotal"
+                        @current-change="handleProcessExcelPageChange"
+                ></el-pagination>
+            </div>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="success" @click="viewprocessingprogress">刷 新</el-button>
+                <el-button @click="processExcelVisible = false">取 消</el-button>
             </span>
         </el-dialog>
     </div>
@@ -223,7 +318,7 @@
                     address: '',
                     name: '',
                     pageIndex: 1,
-                    pageSize: 10
+                    pageSize: 8
 //                pageSize: [100, 200, 300, 400]
                 },
                 tableData: [],
@@ -235,6 +330,7 @@
                 uploadFormVisible:false,
                 foodtypeVisible:false,
                 dialogFormVisible: false,
+                processExcelVisible: false,
                 pageTotal: 0,
                 form: {},
                 idx: -1,
@@ -253,8 +349,19 @@
                 havedistributenames:[],
                 /*保存之前已选择但本次又选择的*/
                 againhavedistributenames:[],
-                checked:true
-
+                checked:true,
+                /*保存文件上传列表*/
+                fileList: [],
+                /*保存文件处理进度数据*/
+                processexceldata:[],
+                processexcelquery: {
+                    address: '',
+                    name: '',
+                    pageIndex: 1,
+                    pageSize: 5,
+                    pageTotal: 0
+                },
+//                uploadUrl:  this.$axios.defaults.baseURL+"/sslibrary/uploadbyexcel"
             };
         },
         created() {
@@ -266,6 +373,7 @@
                 this.$axios.post('/sslibrary/getalllibrary',
                     this.$qs.stringify(
                         {
+                            searchname:this.searchname,
                             pageIndex:this.query.pageIndex,
                             pageSize:this.query.pageSize,
                         })
@@ -278,9 +386,8 @@
                         this.tableData = response.data.data.sysSamplingLibraries;
                         for(var i=0;i<response.data.data.foodTypes.length;i++){
                             this.foodtypes[i] = response.data.data.foodTypes[i].type_name;
-
                         }
-//                        this.pageTotal = response.data.data.pageTotal
+                        this.pageTotal = response.data.data.pageTotal
                     });
             },
             // 触发搜索按钮
@@ -288,7 +395,7 @@
                 this.$set(this.query, 'pageIndex', 1);
                 this.getData();
             },
-            // 删除操作 （假的删除，把ids是否修改置为0,方便记录数据）
+            // 删除操作
             handleDelete(index, row) {
 
                 // 二次确认删除
@@ -355,7 +462,7 @@
                         }
                         this.$set(this.tableData, this.idx, this.form);
                         this.$message.success('修改成功！');
-
+                        this.getData();
                     });
             },
             /*添加信息*/
@@ -454,44 +561,111 @@
                     });
                 this.distributenames = [];
             },
-            /*重置抽检员到抽检账号的分配*/
-            resetaccount(){
+            /*提交表单，会直接跳转到uploadSectionFile*/
+            submitUpload() {
+                this.$refs.upload.submit();
+            },
+            /*实际提交的自定义方法*/
+            uploadSectionFile(file){
+                let formData  = new FormData();
+                // 上传文件对象 名称file与后台控制器参数要一致
+                formData.set('file',file.file);
+                var str = file.file.name;
+                var strs= new Array(); //定义一数组
+                strs= String(str).split(".");
+                if(strs[1]!='xls'&&strs[1]!='xlsx'){
+                    this.$message.error("上传的文件不是excel")
+                    return;
+                }
+                 this.$axios.post('/sslibrary/uploadbyexcel',
+                        formData,
+                    {
+                        headers: {
+                            "Content-type": "multipart/form-data"
+                        },
+                        onUploadProgress:progressEvent => {
+                            let percent=(progressEvent.loaded / progressEvent.total * 100) | 0
+                            //调用onProgress方法来显示进度条，需要传递个对象 percent为进度值
+                            file.onProgress({percent:percent})
 
-                this.$axios.post('/ssaccount/resetsamplingaccount',
+                        }
+                    },
+                )
+                    .then (response => {
+
+                        if(response == null){
+                            return;
+                        }
+                        this.$message.success('上传成功 查看详细情况请点击excel处理进度');
+
+                    });
+            },
+            /*处理删除列表中某个文件*/
+            handleRemove(file, fileList) {
+                this.$message.success("移除"+file.name+"成功")
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            fileexpandLimit(files, fileList){
+                this.$message.error("已上传文件超出限制，请删除后重试！")
+            },
+
+            /*查看excel处理进程*/
+            viewprocessingprogress(){
+                this.processExcelVisible = true;
+                this.$axios.post('/sepp/getall',
                     this.$qs.stringify(
                         {
-                            adminaccount:localStorage.getItem("ms_username"),
-                        }),
-//                    {headers: {'Content-Type': 'contentType:"application/json"'}}
+                            pageIndex:this.processexcelquery.pageIndex,
+                            pageSize:this.processexcelquery.pageSize,
+                        })
                 )
                     .then (response => {
                         if(response == null){
                             return;
                         }
-                        this.getData();
-                        this.$message.success('重置成功');
+                        this.processexceldata = response.data.data.ExcelProcessingProgress;
+                        this.processexcelquery.pageTotal = response.data.data.pageTotal
                     });
             },
-            /*随机分配抽检员到抽检账号*/
-            randomlyassigned(){
-                this.randomVisible = true;
+            /*exce处理进程弹出层分页*/
+            handleProcessExcelPageChange(val){
+                this.$set(this.processexcelquery, 'pageIndex', val);
+                this.viewprocessingprogress();
             },
-            saverandom(){
+            // 删除exce处理进程操作
+            handleprocessexcelDelete(index, row) {
+                // 二次确认删除
+                this.$confirm('确定要删除吗？', '提示', {
+                    type: 'warning'
+                })
+                    .then(() => {
+                    if(row.state == false){
+                        this.$message.error('正在传入数据，不可删除');
+                        return;
+                    }
+                        this.$axios.post('/sepp/deleteexcel',
+                            this.$qs.stringify(
+                                {
+                                    id: row.id,
+                                })
+                        )
+                            .then (response => {
+                                if(response == null){
+                                    return;
+                                }
+                                this.$message.success('删除成功');
+                                this.processexceldata.splice(index, 1);
+                            });
 
-                this.$axios.post('/ssaccount/randomlyassigned',
-                    this.$qs.stringify(
-                        {
-                            adminaccount:localStorage.getItem("ms_username"),
-                        }),
-                )
-                    .then (response => {
-                        if(response == null){
-                            return;
-                        }
 
-                        this.getData();
-                        this.$message.success('随机分配成功');
-                    });
+                    })
+                    .catch(() => {});
+            },
+            /*下载模板*/
+             downLoad() {
+                    window.open(this.$axios.defaults.baseURL+ "/sslibrary/download", '_blank')
             }
         }
     };
@@ -501,7 +675,6 @@
     .handle-box {
         margin-bottom: 20px;
     }
-
     .handle-select {
         width: 120px;
     }
